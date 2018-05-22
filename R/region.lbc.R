7f15ccf028585b26532a2a077698c5b63d470504
Etienne Delay <etienne.delay@cirad.fr>
2018-05-22 22:25:37 +0200

    test depend

--- R/region.lbc.R
+++ R/region.lbc.R
@@ -5,21 +5,21 @@
 #'
 #' @param nothing
 #' @return a charactere vector within frensh regions from leboncoin
-#' @examples 
+#' @examples
 #' my.region <- region.lbc()[7]
 #' @export
 region.lbc <- function(){
   # Page Web à scraper
   HTML_Cible <- "https://www.leboncoin.fr/"
-  
+
   # Encoding de la pagee Web...
-  guess_encoding(read_html(HTML_Cible))
-  
+  readr::guess_encoding(rvest::read_html(HTML_Cible))
+
   # Structure de ma page Web ...
-  Web_Read <- HTML_Cible %>% read_html(encoding = "ISO-8859-1")
-  
+  Web_Read <- HTML_Cible %>% rvest::read_html(encoding = "ISO-8859-1")
+
   # Vérification de l'extraction de la liste des continents de la page Web...
-  Continents <- Web_Read %>% html_nodes("a") %>%
-    html_attr("title")
+  Continents <- Web_Read %>% rvest::html_nodes("a") %>%
+    rvest::html_attr("title")
   return(region <- Continents[34:59])
 }
